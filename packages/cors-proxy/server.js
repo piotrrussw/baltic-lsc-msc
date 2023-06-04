429d64e433982855ae3dde72ea1e19a9e8a879ea
piotrrussw <piotr.wadycki@gmail.com>
2023-06-04 21:05:43 +0200

    init v2

---
+++ packages/cors-proxy/server.js
@@ -0,0 +1,51 @@
+var express = require('express'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    https = require('https'),
+    app = express();
+
+var myLimit = typeof (process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
+console.log('Using limit: ', myLimit);
+
+app.use(bodyParser.json({ limit: myLimit }));
+
+const agentOptions = {
+    host: 'balticlsc.iem.pw.edu.pl',
+    port: '443',
+    path: '/',
+    rejectUnauthorized: false
+};
+
+const agent = new https.Agent(agentOptions);
+
+app.all('*', function (req, res, next) {
+
+    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+
+    if (req.method === 'OPTIONS') {
+        // CORS Preflight
+        res.send();
+    } else {
+        var targetURL = req.header('Target-URL');
+        if (!targetURL) {
+            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+            return;
+        }
+        request({ url: targetURL, method: req.method, agent, json: req.body, headers: { 'Authorization': req.header('Authorization') || '' } },
+            function (error, response, body) {
+                if (error) {
+                    console.error('error: ' + response?.statusCode)
+                }
+                // console.log(response, body, error);
+            }).pipe(res);
+    }
+});
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function () {
+    console.log('Proxy server listening on port ' + app.get('port'));
+});
